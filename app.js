03de91b4d8fc0dfc57183157d3a869b23a85762c
Ayushi Rai <ayushicodes@gmail.com>
2022-09-07 00:34:39 +0530

    score

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const grid = document.querySelector('.grid')
-const scoreDisplay = document.querySelector('#score')
+const scoreDisplay = document.querySelector('.score')
 const blockWidth = 100
 const blockHeight = 20
 const ballDiameter = 20
